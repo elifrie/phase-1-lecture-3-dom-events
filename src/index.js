f108c8d76ada9a691b713fddd11ec9d3fc92c6b6
Eli Friedlander <eli.friedlander@gmail.com>
2023-04-12 16:17:31 -0400

    needed some help

--- src/index.js
+++ src/index.js
@@ -53,17 +53,45 @@ const foods = [
 
 const restaurantMenu = document.getElementById('restaurant-menu')
 
-for(let index = 0; index < foods.length; index++){
+function addFoodImageToMenu(food){
     const image = document.createElement('img')
-    image.src = foods[index].image
+    image.src = food.image
+    image.addEventListener('click',() => {
+        displayFoodDetails(food)
+    })
     restaurantMenu.appendChild(image)
 }
 
-const foodDetailImage = document.querySelector('.detail-image')
-foodDetailImage.src = foods[0].image
+for(let index = 0; index < foods.length; index++){
+    addFoodImageToMenu(foods[index])
+}
 
-const foodName = document.querySelector('.name') 
-foodName.textContent = foods[0].name
+function displayFoodDetails(food){
+        const foodDetailImage = document.querySelector('.detail-image')
+        foodDetailImage.src = food.image
+        
+        const foodName = document.querySelector('.name') 
+        foodName.textContent = food.name
+        
+        const foodDescription = document.querySelector(`#description-display`)
+        foodDescription.textContent = food.description
+}
 
-const foodDescription = document.querySelector(`#description-display`)
-foodDescription.textContent = foods[0].description
+displayFoodDetails(foods[0])
+
+const newFoodForm = document.getElementById('new-food')
+newFoodForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const foodNameValue = document.getElementById('new-name').value
+    const imgaeLinkValue = document.getElementById('new-image').value
+    const descriptionValue = document.getElementById('new-description').value
+    console.log(foodNameValue)
+    console.log(imgaeLinkValue)
+    console.log(descriptionValue)
+    const food = {
+        name: foodNameValue,
+        image: imgaeLinkValue,
+        description: descriptionValue
+    }
+    addFoodImageToMenu(food)
+})
